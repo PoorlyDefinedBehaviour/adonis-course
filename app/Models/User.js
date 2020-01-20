d475d0f686a2d42d0cbdbef76716eaf8f6d6e5e3
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-01-20 19:31:49 -0300

    :sparkles: Update user model

--- app/Models/User.js
+++ app/Models/User.js
@@ -8,17 +8,15 @@ const Hash = use("Hash")
 
 class User extends Model {
   static boot() {
-    super.boot()
-
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
     this.addHook("beforeSave", async (userInstance) => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
     })
+  }
+
+  static get hidden() {
+    return ["password"]
   }
 
   static get traits() {
@@ -28,18 +26,16 @@ class User extends Model {
     ]
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
   tokens() {
     return this.hasMany("App/Models/Token")
+  }
+
+  image() {
+    return this.belongsTo("App/Models/Image")
+  }
+
+  coupons() {
+    return this.belongsToMany("App/Models/Coupon")
   }
 }
 

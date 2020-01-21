f340cd9a028ea87bf260805b92a4e02da2da849c
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-01-21 00:11:42 -0300

    :sparkles: Create AuthController

---
+++ app/Controllers/Http/Auth/AuthController.js
@@ -0,0 +1,15 @@
+"use strict"
+
+class AuthController {
+  async register() {}
+
+  async login() {}
+
+  async logout() {}
+
+  async refreshToken() {}
+
+  async forgotPassword() {}
+}
+
+module.exports = AuthController

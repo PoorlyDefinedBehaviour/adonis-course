234c58a8353ed366f50c4317fff3ffc2e6da952f
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-01-21 00:12:04 -0300

    :sparkles: Create User/Order/Coupon/Discount models

--- app/Models/User.js
+++ app/Models/User.js
@@ -8,6 +8,8 @@ const Hash = use("Hash")
 
 class User extends Model {
   static boot() {
+    super.boot()
+
     this.addHook("beforeSave", async (userInstance) => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
